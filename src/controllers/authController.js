02ec821747cc3ee8eff695015fc7fc4b9508beb1
23CABSHIYAMR <shiyamramesh01@gmail.com>
2025-12-10 14:25:12 +0530

    rmv: domain from cookie

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,6 @@ const COOKIE_OPTIONS = {
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   path: "/",
   maxAge: 60 * 60 * 24,
-  domain: process.env.COOKIE_DOMAIN,
 };
 export const register = async (req, res) => {
   try {

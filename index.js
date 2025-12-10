bb684b74cc8e38f5824d4d4817f2dc85edbe3026
23CABSHIYAMR <shiyamramesh01@gmail.com>
2025-12-10 15:08:59 +0530

    opened cors to debug

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL,/\.vercel\.app$/ ],
+    origin:true,
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
